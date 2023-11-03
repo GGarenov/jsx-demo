a26601d08255de18e1ae4fa84fd0a9d0ad934e51
Georgiy Garenov <g_garenov@outlook.com>
2023-11-03 17:38:07 +0200

    Footer element

--- src/script.js
+++ src/script.js
@@ -1,7 +1,7 @@
-import '../node_modules/react/umd/react.production.min.js';
-import '../node_modules/react-dom/umd/react-dom.production.min.js';
+import "../node_modules/react/umd/react.production.min.js";
+import "../node_modules/react-dom/umd/react-dom.production.min.js";
 
-const rootDomElement = document.getElementById('root');
+const rootDomElement = document.getElementById("root");
 
 const root = ReactDOM.createRoot(rootDomElement);
 
@@ -26,23 +26,25 @@ const root = ReactDOM.createRoot(rootDomElement);
 //     );
 // }
 
-const Footer = () => (
-    <div className='site-footer'>
-        <p>All rights reserved &copy;</p>
-    </div>
-);
+function FooterText() {
+  return React.createElement("p", {}, "All rights reserved R");
+}
+
+function Footer() {
+  return React.createElement("div", { className: "site-footer" }, React.createElement(FooterText));
+}
 
 const headerJSX = (
-    <div>
-        <header className='site-header'>
-            <h1>Hello from JSX!</h1>
-            <h2>The best syntax ever!</h2>
+  <div>
+    <header className="site-header">
+      <h1>Hello from JSX!</h1>
+      <h2>The best syntax ever!</h2>
 
-            <p>something else here</p>
-        </header>
+      <p>something else here</p>
+    </header>
 
-        <Footer />
-    </div>
+    <Footer />
+  </div>
 );
 
 root.render(headerJSX);
